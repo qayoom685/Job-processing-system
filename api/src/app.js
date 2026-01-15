d7aee9437befb8d3e8783984dccb38952aeccdbe
Abdul Qayoom <qayoomk013@gmail.com>
2026-01-15 20:51:14 +0500

    make changes in Database

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
+const db = require('./db/mysql');
 
 app.use(express.json());
+
+app.get('/db-test', async (req, res) => {
+  const [rows] = await db.query('SELECT 1');
+  res.json({ db: 'connected' });
+});
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
+
 
 module.exports = app;
